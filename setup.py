3cf94569d4cdbf838b2679071bf7fd09d17c0258
Kota Mori <kmori05@gmail.com>
2021-09-17 14:18:41 +0900

    update readme

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+
+readmefile = os.path.join(os.path.dirname(__file__), "README.md")
+with open(readmefile) as f:
+    readme = f.read()
 
 setup(
     name='gdriveaudio',
-    version='0.0.2',
+    version='0.0.3',
     description='Play music files on Google Drive',
     author='Kota Mori', 
     author_email='kmori05@gmail.com',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/kota7/gdriveaudio',
     
     packages=[],
